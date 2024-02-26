ddcf3eb7c77dd0e1bf9059078af509d6287298a9
Dhananjay Puranik <dhananjaypuranik@Dhananjays-MacBook-Pro.local>
2024-02-26 23:38:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const dotenv=require("dotenv");
+const cors=require("cors");
+const app=express();
+dotenv.config();
+
+const hotelRouter=require("./routes/hotel.router");
+const hotelDataAddedToDBRouter=require("./routes/dataimport.router");
+const categoryDataAddedToDBRouter = require("./routes/categoryimport.router");
+const categoryRouter = require("./routes/category.router");
+const connectDB=require("./config/dbconfig");
+app.use(cors());
+app.use(express.json());
+connectDB();
+const PORT=3500;
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+})
+app.use("/api/hoteldata",hotelDataAddedToDBRouter);
+app.use("/api/categorydata", categoryDataAddedToDBRouter);
+app.use("/api/hotels",hotelRouter);
+app.use("/api/category", categoryRouter);
+
+mongoose.connection.once("open", () => {
+    console.log("Connected to DB");
+    app.listen(process.env.PORT || PORT, () => {
+      console.log("Server is Up and Running");
+    });
+  });
---
+++ routes/hotel.router.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const router =express.Router();
+
+const Hotel=require("../model/hotel.model");
+
+router.route("/")
+    .get(async(req,res)=>{
+        const hotelCategory=req.query.category
+        try{
+           let hotels
+           if(hotelCategory){
+            const hotels=await Hotel.find({category:hotelCategory});
+        }else{
+            hotels=await Hotel.find({});
+        } 
+            hotels?res.json(hotels):res.status(404).json({message:"No data found"})
+        }catch(err){
+            console.log(err);
+        }
+})
+module.exports=router;
